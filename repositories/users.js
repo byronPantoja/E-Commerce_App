7691da8ce00bee257f0e7236700fdfb6af5f6c3a
byronPantoja <byronpantoja@gmail.com>
2020-04-14 01:08:26 +0800

    Assigning a randomID to users

--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const crypto = require("crypto");
 
 class UsersRepository {
     constructor(filename) {
@@ -23,10 +24,22 @@ class UsersRepository {
     }
 
     async create(attrs) {
+        attrs.id = this.randomId();
+
         const records = await this.getAll();
         records.push(attrs);
 
-        await fs.promises.writeFile(this.filename, JSON.stringify(records));
+        await this.writeAll(records);
+    }
+    async writeAll(records) {
+        await fs.promises.writeFile(
+            this.filename,
+            JSON.stringify(records, null, 2)
+        );
+    }
+
+    randomId() {
+        return crypto.randomBytes(4).toString("hex");
     }
 }
 

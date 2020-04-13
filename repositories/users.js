4939180c78d5026f31e23533d8559719b8313b1f
byronPantoja <byronpantoja@gmail.com>
2020-04-14 00:39:32 +0800

    Adding getAll logic

--- repositories/users.js
+++ repositories/users.js
@@ -16,8 +16,20 @@ class UsersRepository {
 
     async getAll() {
         //Open the file called this.filename
-        read its msContentScript
+        return JSON.parse(
+            await fs.promises.readFile(this.filename, {
+                encoding: "utf8",
+            })
+        );
     }
 }
 
-new UsersRepository("users.json");
+const test = async() => {
+    const repo = new UsersRepository("users.json");
+
+    const users = await repo.getAll();
+
+    console.log(users);
+};
+
+test();

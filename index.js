d51fc7153781f5b997b3bc4eddddca15ba5bd329
byronPantoja <byronpantoja@gmail.com>
2020-04-13 23:32:36 +0800

    Manually creating bodyParser to learn more about middleware

--- index.js
+++ index.js
@@ -15,16 +15,25 @@ app.get("/", (req, res) => {
     `);
 });
 
-app.post("/", (req, res) => {
-    req.on("data", data => {
-        const parsed = data.toString("utf8").split("&");
-        const formData = {};
-        for (let pair of parsed) {
-            const [key, value] = pair.split("=");
-            formData[key] = value;
-        }
-        console.log(formData);
-    });
+const bodyParser = (req, res, next) => {
+    if (req.method === "POST") {
+        req.on("data", data => {
+            const parsed = data.toString("utf8").split("&");
+            const formData = {};
+            for (let pair of parsed) {
+                const [key, value] = pair.split("=");
+                formData[key] = value;
+            }
+            req.body = formData;
+            next();
+        });
+    } else {
+        next();
+    }
+};
+
+app.post("/", bodyParser, (req, res) => {
+    console.log(req.body);
     res.send("Account Created!");
 });
 app.listen(3000, () => {

0fc41ca0abc77e3a9de6b1f1afca064348068569
byronPantoja <byronpantoja@gmail.com>
2020-04-13 23:38:39 +0800

    Removing my bodyParser and replacing it with express/bodyParser

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -15,24 +16,7 @@ app.get("/", (req, res) => {
     `);
 });
 
-const bodyParser = (req, res, next) => {
-    if (req.method === "POST") {
-        req.on("data", data => {
-            const parsed = data.toString("utf8").split("&");
-            const formData = {};
-            for (let pair of parsed) {
-                const [key, value] = pair.split("=");
-                formData[key] = value;
-            }
-            req.body = formData;
-            next();
-        });
-    } else {
-        next();
-    }
-};
-
-app.post("/", bodyParser, (req, res) => {
+app.post("/", bodyParser.urlencoded({ extended: true }), (req, res) => {
     console.log(req.body);
     res.send("Account Created!");
 });

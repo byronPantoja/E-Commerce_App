f5ba9c6969a5acbb10270a4dde8115a8ab772800
byronPantoja <byronpantoja@gmail.com>
2020-04-14 01:32:18 +0800

    Adding Update method to Update user by ID

--- repositories/users.js
+++ repositories/users.js
@@ -52,12 +52,27 @@ class UsersRepository {
         const filteredRecords = records.filter(record => record.id != id);
         await this.writeAll(filteredRecords);
     }
+
+    async update(id, attrs) {
+        const records = await this.getAll();
+        const record = records.find(record => record.id === id);
+
+        if (!record) {
+            throw new Error(`Record with id ${id} not found`);
+        }
+
+        Object.assign(record, attrs);
+        await this.writeAll(records);
+    }
 }
 
 const test = async() => {
     const repo = new UsersRepository("users.json");
 
-    await repo.delete("6f735b50");
+    await repo.update("3aacd87", {
+        email: "test@testemail.com",
+        password: "test",
+    });
 };
 
 test();

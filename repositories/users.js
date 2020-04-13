686e921c6d443a47d6d543f94995ea40169e77bf
byronPantoja <byronpantoja@gmail.com>
2020-04-14 01:14:06 +0800

    Adding getOne to retrieve user by DI

--- repositories/users.js
+++ repositories/users.js
@@ -41,14 +41,19 @@ class UsersRepository {
     randomId() {
         return crypto.randomBytes(4).toString("hex");
     }
+
+    async getOne(id) {
+        const records = await this.getAll();
+        return records.find(record => record.id === id);
+    }
 }
 
 const test = async() => {
     const repo = new UsersRepository("users.json");
-    await repo.create({ email: "test@test.com", password: "password" });
-    const users = await repo.getAll();
 
-    console.log(users);
+    const user = await repo.getOne("6f73550");
+
+    console.log(user);
 };
 
 test();

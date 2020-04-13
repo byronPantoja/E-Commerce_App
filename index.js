9129c248bb33e05cd4cb6cdbffd9fc6b7068c6a7
byronPantoja <byronpantoja@gmail.com>
2020-04-13 22:39:36 +0800

    Created a form element with a method of POST to send user data input

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hello world!");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send(`
+  <div>
+    <form method="POST">
+      <input name="email" placeholder="email" />
+      <input name="password" placeholder="password" />
+      <input name="passwordConfirmation" placeholder="password confirmation" />
+      <button>Sign In</button>
+    </form>
+  </div>
+    `);
+});
+
+app.post("/", (req, res) => {
+    res.send("Account Created!");
+});
+app.listen(3000, () => {
+    console.log("W3 AR3 B0RG");
+});

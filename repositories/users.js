e16fc74541fecb15d70f79956e6af4188ee5720b
byronPantoja <byronpantoja@gmail.com>
2020-04-14 00:49:22 +0800

    Adding create function

--- repositories/users.js
+++ repositories/users.js
@@ -15,18 +15,24 @@ class UsersRepository {
     }
 
     async getAll() {
-        //Open the file called this.filename
         return JSON.parse(
             await fs.promises.readFile(this.filename, {
                 encoding: "utf8",
             })
         );
     }
+
+    async create(attrs) {
+        const records = await this.getAll();
+        records.push(attrs);
+
+        await fs.promises.writeFile(this.filename, JSON.stringify(records));
+    }
 }
 
 const test = async() => {
     const repo = new UsersRepository("users.json");
-
+    await repo.create({ email: "test@test.com", password: "password" });
     const users = await repo.getAll();
 
     console.log(users);

5e13b6810f019ae8849f40a51b719bfcda7a04fb
byronPantoja <byronpantoja@gmail.com>
2020-04-14 01:19:51 +0800

    Adding delete to remove user by ID

--- repositories/users.js
+++ repositories/users.js
@@ -46,14 +46,18 @@ class UsersRepository {
         const records = await this.getAll();
         return records.find(record => record.id === id);
     }
+
+    async delete(id) {
+        const records = await this.getAll();
+        const filteredRecords = records.filter(record => record.id != id);
+        await this.writeAll(filteredRecords);
+    }
 }
 
 const test = async() => {
     const repo = new UsersRepository("users.json");
 
-    const user = await repo.getOne("6f73550");
-
-    console.log(user);
+    await repo.delete("6f735b50");
 };
 
 test();

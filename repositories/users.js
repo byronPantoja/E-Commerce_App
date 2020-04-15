9a67c14193c5767498567b1f6a0852ea2b23f894
byronPantoja <byronpantoja@gmail.com>
2020-04-15 15:03:58 +0800

    Adding hash and salt to password

--- repositories/users.js
+++ repositories/users.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const util = require('util');
 
+const scrypt = util.promisify(crypto.scrypt);
 class UsersRepository {
     constructor(filename) {
         if (!filename) {
@@ -26,12 +28,19 @@ class UsersRepository {
     async create(attrs) {
         attrs.id = this.randomId();
 
+        const salt = crypto.randomBytes(8).toString('hex');
+        const buf = await scrypt(attrs.password, salt, 64);
+
         const records = await this.getAll();
-        records.push(attrs);
+        const record = {
+            ...attrs,
+            password: `${buf.toString('hex')}.${salt}`,
+        };
+        records.push(record);
 
         await this.writeAll(records);
 
-        return attrs;
+        return record;
     }
     async writeAll(records) {
         await fs.promises.writeFile(

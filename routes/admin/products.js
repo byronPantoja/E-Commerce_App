52ee3d8a7a51d75fbc01b81aef059df1d58616da
byronPantoja <byronpantoja@gmail.com>
2020-04-17 20:59:01 +0800

    Creating a product creating form

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const productsRepo = require('../../repositories/products');
+const productsNewTemplate = require('../../views/admin/products/new');
 
 const router = express.Router();
 
 router.get('/admin/products', (req, res) => {});
-router.get('/admin/products/new', (req, res) => {});
+router.get('/admin/products/new', (req, res) => {
+    res.send(productsNewTemplate({}));
+});
 
 module.exports = router;

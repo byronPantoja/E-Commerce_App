821c9eb642080b427e1a680605a81d55e81920fd
byronPantoja <byronpantoja@gmail.com>
2020-04-17 15:07:59 +0800

    Creating product routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(
     })
 );
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
     console.log('W3 AR3 B0RG');

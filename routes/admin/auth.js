b56d30e6f135d56977502269a9417582efdfb98a
byronPantoja <byronpantoja@gmail.com>
2020-04-18 15:15:22 +0800

    Creating middlewares.js file to handleErrors and clean up the routes a bit

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
 
+const { handleErrors } = require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -20,14 +20,9 @@ router.get('/signup', (req, res) => {
 
 router.post(
     '/signup', [requireEmail, requirePassword, requirePasswordConfirmation],
+    handleErrors(signupTemplate),
     async(req, res) => {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            return res.send(signupTemplate({ req, errors }));
-        }
-
-        const { email, password, passwordConfirmation } = req.body;
+        const { email, password } = req.body;
         const user = await usersRepo.create({ email, password });
 
         req.session.userId = user.id;
@@ -47,14 +42,9 @@ router.get('/signin', (req, res) => {
 
 router.post(
     '/signin', [requireEmailExists, requireValidPasswordForUser],
+    handleErrors(signinTemplate),
     async(req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.send(signinTemplate({ errors }));
-        }
-
         const { email } = req.body;
-
         const user = await usersRepo.getOneBy({ email });
 
         req.session.userId = user.id;
--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const { validationResult } = require('express-validator');
 const multer = require('multer');
 
+const { handleErrors } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
@@ -17,12 +17,8 @@ router.get('/admin/products/new', (req, res) => {
 router.post(
     '/admin/products/new',
     upload.single('image'), [requireTitle, requirePrice],
+    handleErrors(productsNewTemplate),
     async(req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.send(productsNewTemplate({ errors }));
-        }
-
         const image = req.file.buffer.toString('base64');
         const { title, price } = req.body;
         await productsRepo.create({ title, price, image });

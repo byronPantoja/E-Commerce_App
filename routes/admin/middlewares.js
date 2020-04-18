356d2d55cc59657515cb00b1d7be9528117fd43b
byronPantoja <byronpantoja@gmail.com>
2020-04-18 15:50:06 +0800

    Adding auth for routes and a redirect if user is not logged in

--- routes/admin/middlewares.js
+++ routes/admin/middlewares.js
@@ -12,4 +12,10 @@ module.exports = {
             next();
         };
     },
+    requireAuth(req, res, next) {
+        if (!req.session.userId) {
+            return res.redirect('/signin');
+        }
+        next();
+    },
 };

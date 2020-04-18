b657bf413283d3d4011b474b80439a0c7f7b37e6
byronPantoja <byronpantoja@gmail.com>
2020-04-19 06:13:25 +0800

    Adding styles and a new route for editing products

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -5,6 +5,7 @@ const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
+const productsEditTemplate = require('../../views/admin/products/edit');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
@@ -32,5 +33,17 @@ router.post(
         res.redirect('/admin/products');
     }
 );
+
+router.get('/admin/products/:id/edit', async(req, res) => {
+    const product = await productsRepo.getOne(req.params.id);
+
+    if (!product) {
+        return res.send('Product not found');
+    }
+
+    res.send(productsEditTemplate({ product }));
+});
+
+router.post('/admin/products/:id/edit', requireAuth, async(req, res) => {});
 
 module.exports = router;
